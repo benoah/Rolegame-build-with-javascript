57fa8d28325b1a274cc1b76123244bcee942747d
benoah <noahbenaas@gmail.com>
2022-04-29 12:12:21 +0200

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import Character from "./Character.js";
 const attack = () => {
   if (wizard.death || orc.death) {
     endGame()
-    
   }
 
   wizard.getDiceHtml()
@@ -13,15 +12,15 @@ const attack = () => {
   orc.takeDamage(wizard.currentDiceScore)
   render()
 }
+
 function endGame(){
   const endMessage = wizard.health === 0 && orc.health === 0 ?
       "No victors - all creatures are dead" :
       wizard.health > 0 ? "The Wizard Wins" :
       "The Orc is Victorious"
-  const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-  document.body.innerHTML = 
-  `<div class="end-game">
-      <h1>Game Over</h1>
+      const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = `<div class="end-game">
+                  <h1>Game Over</h1>
       <h2>${endMessage}</h2>
       <h3 class="end-emoji">${endEmoji}</h3>
   </div>` 
